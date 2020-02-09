cbd3d787c555725354f5a35269237e71dc8a78b7
Steven <smsheets@iastate.edu>
2020-02-09 00:22:50 -0600

    ok

---
+++ README.md
@@ -0,0 +1,4 @@
+### dab
+
+get this on github cuz otherwise you'll have to do on 
+your udrive and this shit is going to take for fucking ever.
---
+++ hello.c
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------
+-					   SE 185: Lab 07 - The DS4 Equalizer                   -
+-	Name:                                                                   -
+- 	Section:                                                                -
+-	NetID:                                                                  -
+-	Date:                                                                   -
+----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+-								Includes									 -
+-----------------------------------------------------------------------------*/
+#include <stdio.h>
+
+/*----------------------------------------------------------------------------
+-	                            Prototypes                                   -
+-----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------
+-	                                Notes                                    -
+-----------------------------------------------------------------------------*/
+// Compile with gcc lab07.c -o lab07 -lncurses
+// Run with ./ds4rd.exe -d 054c:05c4 -D DS4_BT -g -b -j | ./lab07
+
+/*---------------------------------------------------------------------------
+-								Implementation                              -
+----------------------------------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+    printf("yeet");
+
+    return 0;
+}
+
+
+/**
+ * PRE: -39 <= number <= 39
+ * Uses print_chars to graph a number from -39 to 39 on the screen.
+ * You may assume that the screen is 80 characters wide.
+ *
+ * @param number - The amount of characters that are to be printed.
+ * @param current_mode - The current mode the program is in.
+ *                       {0:gyro_roll, 1:gyro_pitch, 2:joy_roll, 3:joy_pitch}
+ */
+void DELETE_THIS(int number, int current_mode)
+{
+    // dab
+}
